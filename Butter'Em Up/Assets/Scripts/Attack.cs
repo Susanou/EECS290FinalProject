22a0187a9e0bdf99088a70d98facc26c804ae0f1
Sulamke <Sulamkegan@gmail.com>
2020-04-06 16:25:24 -0400

    Attack

---
+++ Butter'Em Up/Assets/Scripts/Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(knifeSwing());
+
+        }
+    }
+    private IEnumerator knifeSwing()
+    {
+            _animator.SetBool("Attack", true);
+            yield return new WaitForSeconds(1f);
+            _animator.SetBool("Attack", false);
+
+        
+    }
+}
---
+++ Butter'Em Up/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderingAI : MonoBehaviour {
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    [SerializeField] private GameObject knife;
+    private GameObject _knife;
+
+    private bool _alive;
+
+    void Start() {
+        _alive = true;
+    }
+
+    void Update() {
+        if (_alive)
+        {
+            transform.Translate(Time.deltaTime, speed * Time.deltaTime, 0);
+
+        }
+        }
+
+        public void SetAlive(bool alive) {
+            _alive = alive;
+        }
+    }
+
