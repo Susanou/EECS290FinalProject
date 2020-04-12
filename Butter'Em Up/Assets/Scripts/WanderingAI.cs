56d65fbaf0550dce98177cbbcb9ea4d5e39f7398
Sulamke <Sulamkegan@gmail.com>
2020-04-12 13:36:35 -0400

    need animator for enemy movement

--- Butter'Em Up/Assets/Scripts/WanderingAI.cs
+++ Butter'Em Up/Assets/Scripts/WanderingAI.cs
@@ -7,47 +7,37 @@ public class WanderingAI : MonoBehaviour {
     private float timer;
     public float wanderRadius;
     public float wanderTimer;
-    private Transform target;
+    public float dist;
     private NavMeshAgent agent;
     public Rigidbody2D rigibody;
 
     void Start() {
-        agent = GetComponent<NavMeshAgent>();
-        timer = wanderTimer;
+       
+       
     }
 
-    void Update() {
-
+    void Update()
+    {
         timer += Time.deltaTime;
-
-        if (timer >= wanderTimer)
+        if (timer > wanderTimer)
         {
-            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            Vector2 newPos = Random.insideUnitCircle * dist;
             rigibody.MovePosition(newPos);
             timer = 0;
         }
 
 
+    }
+
+
 
 
 
 
     }
-    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
-    {
-        Vector3 randDirection = Random.insideUnitSphere * dist;
-
-        randDirection += origin;
-
-        NavMeshHit navHit;
-
-        NavMesh.SamplePosition(randDirection, out navHit, 5, -1);
-
-        return navHit.position;
-    }
+  
 
 
-       
-    }
+      
 
 
