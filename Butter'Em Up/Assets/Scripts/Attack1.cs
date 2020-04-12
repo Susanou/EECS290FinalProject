dcdee290efd513245f5fce0ae837c6e927105b2a
Susanou <caos2804@gmail.com>
2020-04-12 12:16:20 +0200

    Modified attack to work with State machine

--- Butter'Em Up/Assets/Scripts/Attack1.cs
+++ Butter'Em Up/Assets/Scripts/Attack1.cs
@@ -10,33 +10,43 @@ public class Attack1 : MonoBehaviour
 
     private string spread;
     private Animator _animator;
+    private PlayerMovement movement;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = this.GetComponent<Animator>();
+        movement = this.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            spread = spread1;
-            Debug.Log(spread);
-            StartCoroutine(knifeSwing());
-        }else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log(spread);
-            spread = spread2;
-            StartCoroutine(knifeSwing());
-        }
+
     }
+
+    public void AttackSpread1()
+    {
+        spread = spread1;
+        Debug.Log(spread);
+        StartCoroutine(knifeSwing());
+    }
+
+    public void AttackSpread2()
+    {
+
+        Debug.Log(spread);
+        spread = spread2;
+        StartCoroutine(knifeSwing());
+    }
+
     private IEnumerator knifeSwing()
     {
         _animator.SetBool("attacking", true);
+        movement.currentSate = PlayerState.attack;
         yield return new WaitForSeconds(0.3f);
         _animator.SetBool("attacking", false);
+        movement.currentSate = PlayerState.walk;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
