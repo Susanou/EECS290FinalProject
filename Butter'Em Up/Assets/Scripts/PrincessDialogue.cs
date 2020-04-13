299aa38fce989a0d7e7b8e8af8cc3359a5435e3c
Mira <mxh781@case.edu>
2020-04-13 16:42:39 -0400

    Add Princess sprite with dialogue

--- Butter'Em Up/Assets/Scripts/PrincessDialogue.cs
+++ Butter'Em Up/Assets/Scripts/PrincessDialogue.cs
@@ -9,6 +9,7 @@ public class PrincessDialogue : MonoBehaviour
     public Text dialogueText;
     public int click = 0; // times key is pressed
     public string[] dialogue;
+    public bool inRange; // character is close enough to Princess
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +20,42 @@ public class PrincessDialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Character left range");
         if (Input.GetKeyDown(KeyCode.K))
         {
-            if (click < dialogue.Length)
+            if (click < 1)
             {
                 dialogueBox.SetActive(true);
                 dialogueText.text = dialogue[click++];
 
             }
-            else
-                dialogueBox.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.K) && inRange)
+        {
+            if (click < 4)
+                dialogueText.text = dialogue[click++];
+            else if (click == 4 && inRange)
+            {
+                dialogueText.text = dialogue[4];
+                dialogueBox.SetActive(true);
+            }
+        }
+        else if (!inRange && click > 3)
+            dialogueBox.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = false;
         }
     }
 }
