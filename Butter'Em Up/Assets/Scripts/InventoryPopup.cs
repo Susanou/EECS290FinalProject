bb5de5edd3ff990e66aa6bbba7646870103e6425
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-14 02:17:33 -0500

    Weird working state - added inventory functionality
    specifically, inventory keybind and prefabs for items

---
+++ Butter'Em Up/Assets/Scripts/InventoryPopup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InventoryPopup : MonoBehaviour
+{
+    [SerializeField] public Canvas inventory;
+    bool inventoryOpen = true; //initializing to true, will sync up on the first use
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryOpen = toggleInventory();
+    }
+
+    bool toggleInventory()
+    {
+        inventory.gameObject.SetActive(inventoryOpen);
+        return !inventoryOpen;
+    }
+}
