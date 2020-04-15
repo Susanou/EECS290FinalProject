5e734c70c46cef6435aabf7b50f52129d8fda36b
Sulamke <Sulamkegan@gmail.com>
2020-04-15 12:41:26 -0400

    Created a Boss Music Trigger

---
+++ Butter'Em Up/Assets/Scripts/TriggerBossMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBossMusic : MonoBehaviour
+{
+    [SerializeField] private GameObject Main;
+    [SerializeField] private GameObject Boss;
+
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger)
+        {
+
+
+            Main.SetActive(false);
+            Boss.SetActive(true);
+
+
+
+        }
+    }
+}
