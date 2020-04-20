0873bb39187917e6b29dbb3ae845357fa1f6a172
Susanou <caos2804@gmail.com>
2020-04-20 19:44:50 +0200

    new befriend

--- Butter'Em Up/Assets/Scripts/Befriend.cs
+++ Butter'Em Up/Assets/Scripts/Befriend.cs
@@ -32,23 +32,42 @@ public class Befriend : MonoBehaviour
         
 
         if(enemy == 0){
-            Debug.Log("You have befriended this bread");
-            
-            befriendjingle.SetActive(true);
-            enemyAnimator.SetBool("friend", true);
-            endmusic.SetActive(false);
-            this.GetComponent<WanderingAI>().gameObject.SetActive(false);
+
+            StartCoroutine(friend());
 
         }
         else if(enemy == 2){
-           
-            killMusic.SetActive(true);
-            enemyAnimator.SetBool("dead", true);
-            endmusic.SetActive(false);
-            Debug.Log("You killed that bread");
-            this.GetComponent<WanderingAI>().gameObject.SetActive(false);
+
+            StartCoroutine(dead());
+
         }
     }
+
+    private IEnumerator friend()
+    {
+        Debug.Log("You have befriended this bread");
+
+        befriendjingle.SetActive(true);
+        enemyAnimator.SetBool("friend", true);
+        endmusic.SetActive(false);
+
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+
+    private IEnumerator dead()
+    {
+        killMusic.SetActive(true);
+        enemyAnimator.SetBool("dead", true);
+        endmusic.SetActive(false);
+        Debug.Log("You killed that bread");
+
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+
+
+
 
     public void hurt(string spread){
         damage[(int)dmg] = spread;
