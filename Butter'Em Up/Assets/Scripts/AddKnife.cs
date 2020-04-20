9536927ef65f9569aff37bccdc72b28bd0d03a00
Sulamke <Sulamkegan@gmail.com>
2020-04-20 13:33:00 -0500

    add knife

---
+++ Butter'Em Up/Assets/Scripts/AddKnife.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddKnife : MonoBehaviour
+{
+    [SerializeField] private GameObject Knife;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger)
+        {
+            Knife.SetActive(true);
+        }
+    }
+
+
+       
+}
