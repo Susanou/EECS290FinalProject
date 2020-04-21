d51ece661f9618bc37530d16d63fb8dfc3f646ea
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-21 12:30:50 -0500

    Partial completion on transporters

---
+++ Butter'Em Up/Assets/Scripts/TransportEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//this is from the 2DGameKit Code, 
+public class TransportEndpoint : MonoBehaviour
+{
+    public enum DestinationTag
+    {
+        A, B, C, D, E, F, G,
+    }
+
+
+    public DestinationTag destinationTag;    // This matches the tag chosen on the TransitionPoint that this is the destination for.
+    [Tooltip("This is the gameobject that has transitioned.  For example, the player.")]
+    public GameObject transitioningGameObject;
+    public UnityEvent OnReachDestination;
+}
---
+++ Butter'Em Up/Assets/Scripts/TransporterScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this is from the 2D GameKit Code with some modifications to fit our use case.
+[RequireComponent(typeof(Collider2D))]
+public class TransporterScript : MonoBehaviour
+{
+   public enum TransitionType
+        {
+            DifferentZone, DifferentNonGameplayScene, SameScene,
+        }
+
+
+        public enum TransitionWhen
+        {
+            ExternalCall, InteractPressed, OnTriggerEnter,
+        }
+
+    
+        [Tooltip("This is the gameobject that will transition.  For example, the player.")]
+        public GameObject transitioningGameObject;
+        [Tooltip("Whether the transition will be within this scene, to a different zone or a non-gameplay scene.")]
+        public TransitionType transitionType;
+        public string newSceneName;
+        [Tooltip("The tag of the SceneTransitionDestination script in the scene being transitioned to.")]
+        public TransportEndpoint.DestinationTag transitionDestinationTag;
+        [Tooltip("What should trigger the transition to start.")]
+        public TransitionWhen transitionWhen;
+        [Tooltip("The player will lose control when the transition happens but should the axis and button values reset to the default when control is lost.")]
+        public bool resetInputValuesOnTransition = true;    
+        bool m_TransitioningGameObjectPresent;
+
+        void Start ()
+        {
+            if (transitionWhen == TransitionWhen.ExternalCall)
+                m_TransitioningGameObjectPresent = true;
+        }
+
+        void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.gameObject == transitioningGameObject)
+            {
+                m_TransitioningGameObjectPresent = true;
+
+                if (transitionWhen == TransitionWhen.OnTriggerEnter)
+                    TransitionInternal ();
+            }
+        }
+
+        void OnTriggerExit2D (Collider2D other)
+        {
+            if (other.gameObject == transitioningGameObject)
+            {
+                m_TransitioningGameObjectPresent = false;
+            }
+        }
+
+        void Update ()
+        {
+
+            if(!m_TransitioningGameObjectPresent)
+                return;
+
+            if (transitionWhen == TransitionWhen.InteractPressed)
+            {
+                TransitionInternal ();
+            }
+        }
+
+        protected void TransitionInternal ()
+        {        
+           SceneController.TransitionToScene (this);
+        }
+
+        public void Transition ()
+        {
+            if(!m_TransitioningGameObjectPresent)
+                return;
+
+            if(transitionWhen == TransitionWhen.ExternalCall)
+                TransitionInternal ();
+        }
+}
