6c927f735814192429bbdaa7f3ea9117972b445c
Susanou <caos2804@gmail.com>
2020-04-24 13:47:35 +0200

    Boss testing

--- Butter'Em Up/Assets/Scripts/Boss Scripts/BossHallah.cs
+++ Butter'Em Up/Assets/Scripts/Boss Scripts/BossHallah.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BossHeallah : MonoBehaviour
+public class BossHallah : MonoBehaviour
 {
 
     public Vector2 maxBossArea;
