58920d88049220568eba7c8848eaa8d3de87a198
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-27 14:14:06 -0500

    End Screen Complete!

--- Butter'Em Up/Assets/Scripts/ToWin.cs
+++ Butter'Em Up/Assets/Scripts/ToWin.cs
@@ -7,11 +7,17 @@ using UnityEngine.SceneManagement;
 public class ToWin : MonoBehaviour
 {
     public GameObject transitioningGameObject;
+    public string newSceneName;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == transitioningGameObject)
         {
-            SceneManager.LoadScene("YouWin");
+            TransitionInternal();
         }
+    }
+
+    protected void TransitionInternal()
+    {
+        SceneManager.LoadScene(newSceneName, LoadSceneMode.Single);
     }
 }
