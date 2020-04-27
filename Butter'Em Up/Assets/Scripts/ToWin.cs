a13a12279dc7133ddce03045379cbb126d59348e
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-27 13:59:42 -0500

    Created Win Stuff

---
+++ Butter'Em Up/Assets/Scripts/ToWin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class ToWin : MonoBehaviour
+{
+    public GameObject transitioningGameObject;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == transitioningGameObject)
+        {
+            SceneManager.LoadScene("YouWin");
+        }
+    }
+}
