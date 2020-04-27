28b1923c96da2358e844f6e38936b34192424ac3
Zubair Mukhi <zubair.mukhi@gmail.com>
2020-04-27 15:07:26 -0500

    scripts

--- Butter'Em Up/Assets/Scripts/InventoryPopup.cs
+++ Butter'Em Up/Assets/Scripts/InventoryPopup.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class InventoryPopup : MonoBehaviour
 {
     [SerializeField] public Canvas inventory;
+    [SerializeField] public Canvas pause;
     bool inventoryOpen = false;
+    bool pauseOpen = false;
 
     // Update is called once per frame
     void Update()
@@ -20,5 +22,12 @@ public class InventoryPopup : MonoBehaviour
         inventoryOpen = !inventoryOpen; //swap state
         inventory.gameObject.SetActive(inventoryOpen); //activate/deactivate
         return inventoryOpen; //new value
+    }
+
+    bool togglePause()
+    {
+        pauseOpen = !pauseOpen; //swap state
+        pause.gameObject.SetActive(pauseOpen); //activate/deactivate
+        return pauseOpen; //new value
     }
 }
